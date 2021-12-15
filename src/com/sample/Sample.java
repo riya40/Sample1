a37a05722d9e7bc9e097e00a560861f0d9c55349
riya40 <priyanunna20@gmail.com>
2021-12-15 12:25:43 +0530

    Added:addition message

--- src/com/sample/Sample.java
+++ src/com/sample/Sample.java
@@ -2,6 +2,13 @@ package com.sample;
 
 public class Sample {
     public static void main(String args[]){
+        //Displaying welcome message
         System.out.println("welcome");
+
+        //additin
+        int a=0,b = 0,c;
+        c=a+b;
+        System.out.println(c);
+
     }
 }
